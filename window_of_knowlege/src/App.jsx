15f0173b41ae732476a5caf1ea501ecba6deddb0
daniil <daniltihomirov946@gmail.com>
2024-11-26 19:24:46 +0300

    add chat

--- window_of_knowlege/src/App.jsx
+++ window_of_knowlege/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Chat from "./Chat/Chat.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+    <Chat />
+    </>
   )
 }
 
